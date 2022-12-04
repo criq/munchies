279710886a96182b9d3339d82cb4b966e92aae94
Jan Jablunka <jablunka@gmail.com>
2022-12-04 11:25:54 +0100

    incremental

--- src/Approach.php
+++ src/Approach.php
@@ -98,7 +98,11 @@ abstract class Approach
 			$totalDailyEnergyExpenditure->getUnit(),
 		))->getInUnit($calculator->getUnits());
 
-		$formula = "totalDailyEnergyExpenditure[{$totalDailyEnergyExpenditure}] * weightGoalQuotient[{$tdeeQuotientValue}] = {$result}";
+		$formula = "
+			totalDailyEnergyExpenditure[{$totalDailyEnergyExpenditure}] * weightGoalQuotient[{$tdeeQuotientValue}]
+			= {$result->getInUnit("kcal")->getAmount()->getValue()} kcal
+			= {$result->getInUnit("kJ")->getAmount()->getValue()} kJ
+		";
 
 		return new QuantityMetric("weightGoalEnergyExpenditure", $result, $formula);
 	}
