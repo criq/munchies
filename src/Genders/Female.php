c51cf69c7af1276400c96e72ecc745d51ee880c6
Jan Jablunka <jablunka@gmail.com>
2022-12-04 17:12:12 +0100

    incremental

--- src/Genders/Female.php
+++ src/Genders/Female.php
@@ -144,40 +144,26 @@ class Female extends \Fatty\Gender
 	 */
 	public function calcReferenceDailyIntakeBonus(Calculator $calculator): QuantityMetric
 	{
-		// Referece Daily Intake (RDI): K doporučenému příjmu se následně mohou přidat energetické bonusy za:
-		// a) kojení - pokud žena kojí, viz. tabulka výše
-		// b) těhotenství - v závislosti na pokročilosti těhotenství…
+		$energy = new Energy(new Amount(0), "kcal");
 
+		// Referece Daily Intake (RDI): K doporučenému příjmu se následně mohou přidat energetické bonusy za:
+
+		// a) kojení - pokud žena kojí, viz. tabulka výše
+
+
+
+		// b) těhotenství - v závislosti na pokročilosti těhotenství…
 		// 1. trimestr - bonus 0 Kcal
 		// 2. trimestr - bonus 300 Kcal
 		// 3. trimestr - bonus 300 Kcal
 
-		// Kdy rozdělení do trimestrů je následovné…
-		// 1. trimestr je 1 - 13. týden těhotenství (počítáme 94 dní)
-		// 2. trimestr je 14 - 27. týden (93 dní)
-		// 3. trimestr je 28 - 42. týden (93 dní)
-
-		// !!! předpokládaným termínem porodu je poslední den 40. týdne těhotenství (takže + 14 dní je konec 42. týdne)
-		// 2023-05-05 - poslední den 40. týdne těhotenství
-		// 2023-05-19 - poslední den 42. týdne těhotenství, konec 3. trimestru
-		// 40 týdnů je 280 dní, 42 týdnů je 294 dní
-
-		// => udělat si prostě knihovnu, která vrátí weekCollection + trimestrCollection s termínama...?
-		// Bonus pak podle referenceDate kalkulačky
-
-		// var_dump($this);
 		$referenceDate = $calculator->getReferenceDate();
-		var_dump($referenceDate);
+		$trimester = $this->getPregnancy()->getCurrentTrimester($referenceDate);
+		if (in_array($trimester->getIndex(), [2, 3])) {
+			$energy = $energy->modify(new Energy(new Amount(300), "kcal"));
+		}
 
-		var_dump($this->getPregnancy());
-		// var_dump($this->getPregnancy()->getConceptionDate());
-		// var_dump($this->getPregnancy()->getWeeks());
-		var_dump($this->getPregnancy()->getTrimesters());
-		var_dump($this->getPregnancy()->getChildbirthDate());
-		var_dump($this->getPregnancy()->getCurrentWeek($referenceDate));
-		var_dump($this->getPregnancy()->getCurrentTrimester($referenceDate));
-
-		return new QuantityMetric("referenceDailyIntakeBonus", new Energy(new Amount(0), "kJ"));
+		return new QuantityMetric("referenceDailyIntakeBonus", $energy);
 	}
 
 	/*****************************************************************************
