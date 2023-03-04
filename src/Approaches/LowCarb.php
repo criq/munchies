dee5f66c31993295ce5e21463aca709242e20293
Jan Jablunka <jablunka@gmail.com>
2023-03-04 12:15:14 +0100

    incremental

--- src/Approaches/LowCarb.php
+++ src/Approaches/LowCarb.php
@@ -5,7 +5,10 @@ namespace Fatty\Approaches;
 use Fatty\Amount;
 use Fatty\Calculator;
 use Fatty\Energy;
+use Fatty\Metrics\GoalNutrientsCarbsMetric;
+use Fatty\Metrics\GoalNutrientsFatsMetric;
 use Fatty\Metrics\MetricResultCollection;
+use Fatty\Metrics\QuantityMetricResult;
 use Fatty\Nutrients;
 use Fatty\Nutrients\Carbs;
 use Fatty\Nutrients\Fats;
@@ -20,39 +23,55 @@ class LowCarb extends \Fatty\Approach
 
 	public function calcGoalNutrients(Calculator $calculator): MetricResultCollection
 	{
-		$nutrients = new Nutrients;
-		$nutrients->setProteins($this->calcGoalNutrientsProteins($calculator)->getResult());
+		$carbsResult = new QuantityMetricResult(new GoalNutrientsCarbsMetric);
+		$fatsResult = new QuantityMetricResult(new GoalNutrientsFatsMetric);
+		$proteinsResult = $this->calcGoalNutrientsProteins($calculator);
 
-		$wgee = $calculator->calcWeightGoalEnergyExpenditure();
+		$wgeeResult = $calculator->calcWeightGoalEnergyExpenditure();
+		$carbsResult->addErrors($wgeeResult->getErrors());
+		$fatsResult->addErrors($wgeeResult->getErrors());
 
-		// Sacharidy, maximum je 90 g.
-		$carbs = Carbs::createFromEnergy(
-			new Energy(
-				new Amount(
-					$wgee->getResult()->getInBaseUnit()->getAmount()->getValue() * .2,
-				),
-				Energy::getBaseUnit(),
-			),
-		);
+		if (!$carbsResult->hasErrors() && !$fatsResult->hasErrors() && !$proteinsResult->hasErrors()) {
+			$nutrients = new Nutrients;
 
-		if ($carbs->getInUnit("g")->getAmount()->getValue() > static::CARBS_MAX) {
-			$carbs = new Carbs(new Amount(static::CARBS_MAX));
-		}
+			$nutrients->setProteins($proteinsResult->getResult());
 
-		$nutrients->setCarbs($carbs);
-
-		// Tuky.
-		$nutrients->setFats(
-			Fats::createFromEnergy(
+			// Sacharidy, maximum je 90 g.
+			$carbs = Carbs::createFromEnergy(
 				new Energy(
 					new Amount(
-						$wgee->getResult()->getInBaseUnit()->getAmount()->getValue() - $nutrients->getEnergy()->getInBaseUnit()->getAmount()->getValue()
+						$wgeeResult->getResult()->getInUnit(Energy::getBaseUnit())->getNumericalValue() * .2,
 					),
 					Energy::getBaseUnit(),
 				),
-			),
-		);
+			);
 
-		return $nutrients;
+			if ($carbs->getInUnit("g")->getAmount()->getValue() > static::CARBS_MAX) {
+				$carbs = new Carbs(new Amount(static::CARBS_MAX));
+			}
+
+			$nutrients->setCarbs($carbs);
+
+			// Tuky.
+			$fats = Fats::createFromEnergy(
+				new Energy(
+					new Amount(
+						$wgeeResult->getResult()->getInUnit(Energy::getBaseUnit())->getNumericalValue() - $nutrients->getEnergy()->getInBaseUnit()->getAmount()->getValue()
+					),
+					Energy::getBaseUnit(),
+				),
+			);
+			$nutrients->setFats($fats);
+
+			$carbsResult->setResult($nutrients->getCarbs());
+			$fatsResult->setResult($nutrients->getFats());
+			$proteinsResult->setResult($nutrients->getProteins());
+		}
+
+		return new MetricResultCollection([
+			$carbsResult,
+			$fatsResult,
+			$proteinsResult,
+		]);
 	}
 }
