c290c0b891ddbc8022c217faacd9509640b6ffc3
Jan Jablunka <jablunka@gmail.com>
2023-05-01 12:53:35 +0200

    incremental

--- src/Approaches/LowEnergy.php
+++ src/Approaches/LowEnergy.php
@@ -48,7 +48,7 @@ class LowEnergy extends \Fatty\Approach
 	{
 		return new MetricResultCollection([
 			(new QuantityMetricResult(new GoalNutrientsCarbsMetric))->setResult((new static)->getDefaultCarbs()),
-			(new QuantityMetricResult(new GoalNutrientsFatsMetric))->setResult((new static)->getDefaultFats())
+			(new QuantityMetricResult(new GoalNutrientsFatsMetric))->setResult((new static)->getDefaultFats()),
 			(new QuantityMetricResult(new GoalNutrientsProteinsMetric))->setResult((new static)->getDefaultProteins()),
 		]);
 	}
